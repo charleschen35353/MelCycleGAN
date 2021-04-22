7527bd81b19f9e439338ed937bd09d13c107790f
charleschen35353 <lc3533@columbia.edu>
2021-04-22 17:47:29 +0000

    renamed

--- run_eval.sh
+++ run_eval.sh
@@ -1,4 +1,4 @@
-pretrained_path=MELGANVC-best
+pretrained_path=BEST_MODEL
 source_path=./dataset/cmu_us_ksp_arctic/wav/arctic_a0003.wav
 target_path=./dataset/cmu_us_bdl_arctic/wav/arctic_a0003.wav
 
